10c008df8b4e3b38a446091f043dbfce7424550a
Roman Kosenko <Roman.Kosenko@gmail.com>
2015-11-04 11:56:13 +0000

    NodeConductor extensions autodiscover
    - NC-890

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     },
     entry_points={
         'nodeconductor_extensions': (
-            'nodeconductor_sugarcrm = nodeconductor_sugarcrm.urls',
+            'nodeconductor_sugarcrm = nodeconductor_sugarcrm.extension:SugarCRMExtension',
         ),
     },
     # tests_require=tests_requires,
