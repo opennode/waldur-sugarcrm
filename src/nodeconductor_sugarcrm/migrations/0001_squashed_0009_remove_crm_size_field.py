e113f8e2e2010d53afff35fe1a094355a80cbda7
Victor Mireyev <victor@opennodecloud.com>
2016-09-14 14:19:17 +0300

    Migrated to Django 1.8 UUIDField (NC-1214)

--- src/nodeconductor_sugarcrm/migrations/0001_squashed_0009_remove_crm_size_field.py
+++ src/nodeconductor_sugarcrm/migrations/0001_squashed_0009_remove_crm_size_field.py
@@ -8,7 +8,7 @@ import nodeconductor.core.models
 import django.db.models.deletion
 import django.utils.timezone
 import nodeconductor.logging.loggers
-import uuidfield.fields
+import nodeconductor.core.fields
 import django.core.validators
 import model_utils.fields
 import nodeconductor.core.validators
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=150, verbose_name='name', validators=[nodeconductor.core.validators.validate_name])),
-                ('uuid', uuidfield.fields.UUIDField(unique=True, max_length=32, editable=False, blank=True)),
+                ('uuid', nodeconductor.core.fields.UUIDField()),
                 ('settings', models.ForeignKey(to='structure.ServiceSettings')),
                 ('customer', models.ForeignKey(to='structure.Customer')),
                 ('available_for_all', models.BooleanField(default=False, help_text='Service will be automatically added to all customers projects if it is available for all')),
@@ -78,7 +78,7 @@ class Migration(migrations.Migration):
                 ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
                 ('description', models.CharField(max_length=500, verbose_name='description', blank=True)),
                 ('name', models.CharField(max_length=150, verbose_name='name', validators=[nodeconductor.core.validators.validate_name])),
-                ('uuid', uuidfield.fields.UUIDField(unique=True, max_length=32, editable=False, blank=True)),
+                ('uuid', nodeconductor.core.fields.UUIDField()),
                 ('backend_id', models.CharField(max_length=255, blank=True)),
                 ('start_time', models.DateTimeField(null=True, blank=True)),
                 ('state', django_fsm.FSMIntegerField(default=1, help_text='WARNING! Should not be changed manually unless you really know what you are doing.', choices=[(1, 'Provisioning Scheduled'), (2, 'Provisioning'), (3, 'Online'), (4, 'Offline'), (5, 'Starting Scheduled'), (6, 'Starting'), (7, 'Stopping Scheduled'), (8, 'Stopping'), (9, 'Erred'), (10, 'Deletion Scheduled'), (11, 'Deleting'), (13, 'Resizing Scheduled'), (14, 'Resizing'), (15, 'Restarting Scheduled'), (16, 'Restarting')])),
